9b6a423fe12d725a45d03936ccd019725c7b3708
Magpie <edchavez821@gmail.com>
2020-06-06 20:17:52 -0400

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import digitalNomad from "../images/landing/undraw_digital_nomad_9kgl.svg";
 
 function IndexPage() {
   return (
@@ -14,9 +14,9 @@ function IndexPage() {
 
       <section className="text-center">
         <img
-          alt="Cat and human sitting on a couch"
+          alt="Young, tech-savvy person"
           className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
+          src={digitalNomad}
         />
 
         <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
