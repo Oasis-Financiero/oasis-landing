8977dcff456f7984d3c3cd091c9d5674dcc9e897
Magpie <edchavez821@gmail.com>
2021-04-26 20:48:27 -0500

    Responsive navbar

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,6 @@ import linkedin from "../images/social/linkedin-dark.svg";
 
 function Header() {
 
-  const fontColor = {
-    color: '#3A558D'
-  }
-
   const [isExpanded, toggleExpansion] = useState(false);
   /* const { site } = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -23,6 +19,76 @@ function Header() {
       }
     }
   `); */
+
+  const links = 
+    [
+      {
+        route: `/`,
+        title: `Inicio`,
+      },
+      {
+        route: "/nosotros",
+        title: `Sobre nosotros`,
+      },
+      {
+        route: "/compara",
+        title: `Nuestras herramientas`,
+      },
+      {
+        route: "/blog",
+        title: `Blog`,
+      },
+    ].map((link) => (
+      <Link
+        className={`${headerStyles.navLink} no-underline`}
+        key={link.title}
+        to={link.route}
+      >
+        {link.title}
+      </Link>
+    ))
+
+  const social =
+      <div id={headerStyles.iconsTray}>
+        <a
+          className="font-bold text-white no-underline"
+          href="https://facebook.com/oasisfinanciero"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="Facebook icon"
+            className={headerStyles.socialIcon}
+            src={facebook}
+          />
+        </a>
+
+        <a
+          className="font-bold text-white no-underline"
+          href="https://twitter.com/oasisfintech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="Twitter icon"
+            className={headerStyles.socialIcon}
+            src={twitter}
+          />
+        </a>
+
+        <a
+          className="font-bold text-white no-underline"
+          href="https://www.linkedin.com/company/oasisfinanciero/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="Linkedin icon"
+            className={headerStyles.socialIcon}
+            src={linkedin}
+          />
+        </a>
+      </div>
 
   return (
     <header className={`${headerStyles.header}`}>
@@ -37,42 +103,16 @@ function Header() {
         </div>
 
         <nav
-          style={fontColor}
-          id={headerStyles.nav}
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } `}
+          id={headerStyles.navBar}
         >
-          {[
-            {
-              route: `/`,
-              title: `Inicio`,
-            },
-            {
-              route: "/nosotros",
-              title: `Sobre nosotros`,
-            },
-            {
-              route: "/compara",
-              title: `Nuestras herramientas`,
-            },
-            {
-              route: "/blog",
-              title: `Blog`,
-            },
-          ].map((link) => (
-            <Link
-              className={`${headerStyles.navLink} no-underline`}
-              key={link.title}
-              to={link.route}
-            >
-              {link.title}
-            </Link>
-          ))}
-
+          {links}
+        </nav>
+        
+        <div id={headerStyles.buttonWrapper}>
           <button
-          className="flex items-center block px-3 py-2 text-black border border-black rounded md:hidden"
-          onClick={() => toggleExpansion(!isExpanded)}
+            id={headerStyles.navButton}
+            className="flex items-center block px-3 py-2 text-black border border-black rounded"
+            onClick={() => toggleExpansion(!isExpanded)}
           >
             <svg
               className="w-3 h-3 fill-current"
@@ -83,51 +123,20 @@ function Header() {
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
-        </nav>
-
-        <div id={headerStyles.social}>
-          <div id={headerStyles.iconsTray}>
-            <a
-              className="font-bold text-white no-underline"
-              href="https://facebook.com/oasisfinanciero"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt="Facebook icon"
-                className={headerStyles.socialIcon}
-                src={facebook}
-              />
-            </a>
-
-            <a
-              className="font-bold text-white no-underline"
-              href="https://twitter.com/oasisfintech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt="Twitter icon"
-                className={headerStyles.socialIcon}
-                src={twitter}
-              />
-            </a>
-
-            <a
-              className="font-bold text-white no-underline"
-              href="https://www.linkedin.com/company/oasisfinanciero/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt="Linkedin icon"
-                className={headerStyles.socialIcon}
-                src={linkedin}
-              />
-            </a>
-          </div>
         </div>
+  
+        <div id={headerStyles.navSocial}>
+          {social}
+        </div>
+
       </div>
+
+      <div id={isExpanded ? headerStyles.navDropExpanded : headerStyles.navDropHidden}>
+        {links}
+        {social}
+      </div>
+
+
     </header>
   );
 }
