f0b5d0c2f9c2ed863dcba5b8f9b5581245c4e54a
Magpie <edchavez821@gmail.com>
2021-04-26 22:59:51 -0500

    Homepage media queries 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 
-import { graphql } from "gatsby"
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import PropTypes from "prop-types";
