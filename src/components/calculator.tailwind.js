325170e59c01bdb0bc3a0396a761dd8e9df2360d
Asael Hernandez <zanduva@gmail.com>
2023-06-14 14:19:32 -0600

    css tailwind to box container

--- src/components/calculator.tailwind.js
+++ src/components/calculator.tailwind.js
@@ -1,9 +1,9 @@
 
 
 const tailwind = {
-     box : "shadow-neutral-700 shadow-lg border-2 flex flex-col gap-0 h-[526px] border-[#A86EFF] rounded-[28px] md:w-[506px] md:h-[418px]",
-     container : "flex flex-col justify-center items-center p-5 md:grid md:grid-cols-2 md:place-items-center md:gap-5 md:pt-8",
-     titleContainer: "flex flex-col md:gap-1",
+     box : "shadow-neutral-700 shadow-lg border-2 flex flex-col h-[536px] border-[#A86EFF] rounded-[28px] md:w-[506px] md:h-[418px]",
+     container : "flex flex-col justify-center items-center p-2 md:grid md:grid-cols-2 md:place-items-center md:gap-4 md:pt-8",
+     titleContainer: "flex flex-col",
      dropdown: 'flex pb-2'
 }
 
