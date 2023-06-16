fb30d1c5f45cade110d47e62f3dd514daf44ed53
Asael Hernandez <zanduva@gmail.com>
2023-06-16 12:15:22 -0600

    confirmbutton integrate

---
+++ src/components/styled/ConfirmButton/AppButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './AppButton.css'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+
+const AppButton = () => {
+    return (
+        <>
+            <div>
+                <button
+                    id="button"
+                    className={`flex flex-row justify-center items-center w-[214px] p-2`}
+                >
+                    Calcular préstamo
+                    <MdOutlineKeyboardArrowRight
+                        size={25}
+                    />
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default AppButton
