433ac7d854c321453b7d047e6d14aa7d46a90cd1
Asael Hernandez <zanduva@gmail.com>
2023-06-20 15:28:42 -0600

    style

--- src/pages/compara.js
+++ src/pages/compara.js
@@ -12,47 +12,50 @@ const Compara = () => {
 
     return (
         <Layout page='compara'>
-            <div className='flex flex-col justify-center md:flex md:justify-center md:items-center'>
-                <div className="md:flex md:flex-col md:justify-center md:items-start md:p-[100px]">
-                    <h1 id={style.title}>Préstamos personales para <span id={style.word}>surfear</span> las emergencias</h1>
-                    <div className="flex flex-col justify-center items-center p-2 md:flex md:flex-row md:gap-4">
-                        <Calculator />
-                        <div className="max-md:hidden md:w-[503px] md:h-[497px] md:relative md:bottom-3">
-                            <img
-                                src={wallet}
-                                alt="wallet"
-                            />
+            <div className="flex flex-col gap-10 md:gap-0">
+
+                <div className='flex flex-col justify-center md:flex md:justify-center md:items-center'>
+                    <div className="md:flex md:flex-col md:justify-center md:items-start md:p-[100px]">
+                        <h1 id={style.title}>Préstamos personales para <span id={style.word}>surfear</span> las emergencias</h1>
+                        <div className="flex flex-col justify-center items-center p-2 md:flex md:flex-row md:gap-4">
+                            <Calculator />
+                            <div className="max-md:hidden md:w-[503px] md:h-[497px] md:relative md:bottom-3">
+                                <img
+                                    src={wallet}
+                                    alt="wallet"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
 
 
 
-            <div id={style.faqc} className="md:p-10">
-                <h1 className="flex justify-center" id={style.faq}>FAQs</h1>
-                <AppFaq />
-            </div>
+                <div id={style.faqc} className="md:p-10">
+                    <h1 className="flex justify-center" id={style.faq}>FAQs</h1>
+                    <AppFaq />
+                </div>
 
 
-            <div className="flex flex-col justify-center items-center">
-                
-                <h1 className="text-center">Compara nuestros <br></br>productos <span id={style.word}>financieros</span></h1>
+                <div className="w-full flex flex-col justify-center items-center">
 
-                <div className="flex p-8 gap-2">
-                    <div>
-                        <AppSecondaryButton />
+                    <h1 className="text-center" id={style.titles}>Compara nuestros <br></br>productos <span id={style.word}>financieros</span></h1>
+
+                    <div className="flex justify-center p-8 gap-2 w-full">
+                        <div>
+                            <AppSecondaryButton />
+                        </div>
+
+                        <div>
+                            <AppSecondaryButton />
+                        </div>
                     </div>
 
-                    <div>
-                        <AppSecondaryButton />
-                    </div>
                 </div>
 
             </div>
-
         </Layout>
     )
 }
