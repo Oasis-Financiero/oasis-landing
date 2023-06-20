4800bbcb9a492c62c2d32fdd5401750d3109845f
Asael Hernandez <zanduva@gmail.com>
2023-06-20 12:33:11 -0600

    identation

--- src/components/styled/FAQs/AppFaq.js
+++ src/components/styled/FAQs/AppFaq.js
@@ -4,37 +4,6 @@ import AppAccordion from '../Accordion/Accordion';
 const AppFaq = () => {
 
 
-    const menuItems = [
-        {
-            question: '¿Cómo funcionan los préstamos personales?',
-            answer:
-                " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        },
-        {
-            question: '¿Cómo obtener un préstamo?',
-            answer:
-                " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        },
-        {
-            question: '¿Cómo se calcula el CAT anual?',
-            answer:
-                " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        }
-    ];
-
-    const menuItems2 = [
-        {
-            question: '¿Cuáles son los requisitos para un préstamo personal?',
-            answer:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        },
-        {
-            question: '¿Puedo pagar mi préstamo de forma anticipada?',
-            answer:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        }
-    ]
-
     return (
         <div className='md:flex md:justify-center'>
             <div className='md:w-[470px]'>
