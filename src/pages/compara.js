d5612b8082c6d5ac7b737f3706388c72994a6696
Asael Hernandez <zanduva@gmail.com>
2023-06-26 23:00:50 -0600

    cards seguros

--- src/pages/compara.js
+++ src/pages/compara.js
@@ -11,7 +11,7 @@ import iconSeguro from '../images/herramientas/ico-seguro-auto.svg'
 import iconPrestamo from '../images/herramientas/ico-prestamos-personales.svg'
 import AppLinedCard from "../components/styled/LinedCard/LinedCard";
 import RelevantPoints from "../components/styled/RelevantPoints/RelevantPoints";
-import { Hidden } from "@mui/material";
+import CardTypeSecure from "../components/styled/CardTypeSecure/CardTypeSecure";
 
 
 
@@ -79,8 +79,8 @@ const Compara = () => {
                 </div>
 
 
-                <div className={`bg-[${colors.fdoGris}]`}>
-                    <RelevantPoints/>
+                <div className={`bg-[${colors.fdoGris}] flex justify-center`}>
+                    <CardTypeSecure/>
                 </div>
 
             </div>
