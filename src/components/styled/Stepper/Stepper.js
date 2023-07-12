3ad1b74a7270643448241f268607a408be450968
Asael Hernandez <zanduva@gmail.com>
2023-07-12 13:31:57 -0600

    iconProps

--- src/components/styled/Stepper/Stepper.js
+++ src/components/styled/Stepper/Stepper.js
@@ -2,75 +2,85 @@ import React, { useState } from "react";
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import { createTheme } from "@mui/material";
 import colors from "../../../constants/colors";
 import AppButton from "../ConfirmButton/AppButton";
+import { BsCircleFill } from 'react-icons/bs'
 
+const StepIcon = (props) => {
+    const { active, completed, className } = props;
 
+    const iconStyleActive = {
+        fill: active ? colors.resalte1 : 'transparent',
+        border: active ? 'none' : '1px solid gray',
+    };
+
+    const iconStyleCompleted = {
+        fill: colors.resalte1
+    }
+
+    return (
+        <div className={className}>
+            {
+                completed ?
+                    < BsCircleFill style={iconStyleCompleted} className="rounded-[50%]"/>
+                    :
+                    < BsCircleFill style={iconStyleActive} className="rounded-[50%]"/>
+            }
+        </div>
+    );
+};
 
 export const AppStepper = ({ steps }) => {
-
-    const [isCompleted, setIsCompleted] = useState(false)
-    const [stepsActive, setStepsActive] = useState(0)
-
+    const [isCompleted, setIsCompleted] = useState(false);
+    const [stepsActive, setStepsActive] = useState(0);
 
     const onHandleCompleted = () => {
         if (stepsActive !== steps.length) {
-            setStepsActive(stepsActive + 1)
-        } if (stepsActive === steps.length - 1)
-            setIsCompleted(true)
-    }
+            setStepsActive(stepsActive + 1);
+        }
+        if (stepsActive === steps.length - 1) {
+            setIsCompleted(true);
+        }
+    };
 
     const onBackHandleCompleted = () => {
         if (stepsActive !== 0) {
-            setStepsActive(stepsActive - 1)
-            setIsCompleted(false)
+            setStepsActive(stepsActive - 1);
+            setIsCompleted(false);
         }
-    }
-
+    };
 
     return (
         <div className="md:w-[60%]">
-
             <Stepper activeStep={stepsActive} alternativeLabel>
                 {steps.map((label) => (
-                    <Step key={label} sx={{
-                        '& .MuiStepLabel-root .Mui-completed': {
-                            color: colors.resalte1,
-                        },
-                        '& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel':
-                        {
-                            color: colors.resalte1,
-                            font: 'normal normal 600 16px/17px Inter'
-                        },
-                        '& .MuiStepLabel-root .Mui-active': {
-                            color: colors.resalte1,
-                        },
-                        '& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel':
-                        {
-                            color: colors.brand1,
-                            font: 'normal normal 600 16px/17px Inter'
-                        },
-                        '& .MuiStepLabel-label.MuiStepLabel-alternativeLabel':
-                        {
-                            font: 'normal normal 600 16px/17px Inter',
-                        },
-                        '& .MuiStepLabel-root .Mui-active .MuiStepIcon-text': {
-                            fill: 'white',
-                        },
+                    <Step key={label} completed={isCompleted}
+                        sx={{
+                            '& .MuiStepLabel-label.MuiStepLabel-alternativeLabel':
+                            {
+                                font: 'normal normal 600 16px/17px Inter',
+                            },
 
-                    }}
-                        completed={isCompleted}
-
-                    >
-                        <StepLabel className="text-center">{label}</StepLabel>
+                        }}>
+                        <StepLabel
+                            className="text-center"
+                            StepIconComponent={StepIcon}
+                            active={stepsActive}
+                            completed={isCompleted}
+                        >
+                            {label}
+                        </StepLabel>
                     </Step>
                 ))}
             </Stepper>
-            <div onClick={() => onHandleCompleted()}><AppButton /></div>
-            <div onClick={() => onBackHandleCompleted()}><AppButton /></div>
+            <div onClick={onHandleCompleted}>
+                <AppButton />
+            </div>
+            <div onClick={onBackHandleCompleted}>
+                <AppButton />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default AppStepper
+export default AppStepper;
