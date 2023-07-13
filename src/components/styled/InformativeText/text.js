a244015d3a6009e75b6bdb770498c652d0d418a7
Asael Hernandez <zanduva@gmail.com>
2023-07-13 14:26:58 -0600

    informative text car secure

---
+++ src/components/styled/InformativeText/text.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+const text2 = "Recuerda que un seguro de auto ofrece tranquilidad y seguridad. Con la cobertura adecuada, puedes sentirte tranquilo sabiendo que, en caso de un accidente, estarás respaldado y protegido. También brinda asistencia en carretera, lo que significa que recibirás ayuda en caso de avería o necesidad de remolque."
+const text1 = (<span>Un seguro de auto es de vital importancia debido a los numerosos beneficios que brinda a los propietarios de vehículos. En primer lugar, proporciona protección financiera en caso de accidentes o daños a tu automóvil. Los accidentes pueden ocurrir en cualquier momento y, sin un seguro adecuado, <b>podrías enfrentar gastos inesperados y abrumadores.</b> Un seguro de auto te protege contra responsabilidades legales y cubre los gastos de reparación de tu vehículo, así como los daños a terceros.</span>)
+
+export { text1, text2 }
