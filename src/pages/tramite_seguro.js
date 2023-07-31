338f640ded7b9bfbaefa552114c7ec1da82e65c0
Asael Hernandez <zanduva@gmail.com>
2023-07-31 09:10:02 -0600

    nad

--- src/pages/tramite_seguro.js
+++ src/pages/tramite_seguro.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout";
 import AppStepper from "../components/styled/Stepper/Stepper";
+import AppSelect from '../components/styled/Dropdown/Dropdown'
+import AppTextBox from '../components/styled/TextBox/TextBox'
+import colors from "../constants/colors";
 
 const Tramite = () => {
 
     const steps = ["Tipo de seguro y datos del vehiculo", "Datos Solicitante", "Confirmar Solicitud"]
+
+
 
     return (
         <Layout page='tramite'>
@@ -13,6 +18,50 @@ const Tramite = () => {
                 <AppStepper
                     steps={steps}
                 />
+
+                <div className="flex flex-col justify-center items-center">
+                    <h2>Tipo de seguro y datos de tu vehiculo</h2>
+                    <div></div>
+
+                    <div >
+                        <div>
+
+                        </div>
+
+                        <div className="flex flex-col justify-center">
+                            <div className="grid grid-cols-2 gap-1">
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Tipo de vehiculo"
+                                    width="273px"
+                                />
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Marca"
+                                />
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Modelo"
+                                />
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Anio"
+                                />
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Costo vehiculo (valor factura)"
+                                />
+                                <AppSelect
+                                    items={["hola", "adios"]}
+                                    tagLabel="Numero pasajeros"
+                                />
+                            </div>
+                            <div className="w-full">
+                                <textarea className="w-[556px] h-[96px]" style={{ border: `2px solid ${colors.resalte1}` }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
