f7b1dda0def696e19eb1aa57fff5adbd06d25ea6
Asael Hernandez <zanduva@gmail.com>
2023-08-10 13:25:19 -0600

    firebase

--- firebase.js
+++ firebase.js
@@ -7,24 +7,24 @@ import { getFirestore } from "firebase/firestore";
 
 //PRUEBA CUENTA ASAEL
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDk5dSFrIyz33-pEsVsJOaZL5VJAXxdsH4",
-    authDomain: "oasis-financiero.firebaseapp.com",
-    projectId: "oasis-financiero",
-}
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDk5dSFrIyz33-pEsVsJOaZL5VJAXxdsH4",
+//     authDomain: "oasis-financiero.firebaseapp.com",
+//     projectId: "oasis-financiero",
+// }
 
 
 // PRODUCCION OASIS FINAN!!!!!
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAE8Y7oSOTGOSzVByNt9WroQ432-H58fJo",
-//     authDomain: "oasis-financiero-7a7e9.firebaseapp.com",
-//     projectId: "oasis-financiero-7a7e9",
-//     storageBucket: "oasis-financiero-7a7e9.appspot.com",
-//     messagingSenderId: "662657643593",
-//     appId: "1:662657643593:web:a075d1a33fd361a6c37b98",
-//     measurementId: "G-YTXSXD213J"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyAE8Y7oSOTGOSzVByNt9WroQ432-H58fJo",
+    authDomain: "oasis-financiero-7a7e9.firebaseapp.com",
+    projectId: "oasis-financiero-7a7e9",
+    storageBucket: "oasis-financiero-7a7e9.appspot.com",
+    messagingSenderId: "662657643593",
+    appId: "1:662657643593:web:a075d1a33fd361a6c37b98",
+    measurementId: "G-YTXSXD213J"
+  };
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
