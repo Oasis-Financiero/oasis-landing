7b4d008ba4805b1b07ab97d6f27c37a8a4185dd1
OasisFintech <63435165+OasisFintech@users.noreply.github.com>
2023-11-06 14:06:31 -0600

    Update prestamos-personales.js

--- src/pages/compara/prestamos-personales.js
+++ src/pages/compara/prestamos-personales.js
@@ -16,8 +16,7 @@ import ContactForm from "../../components/styled/ContactForm/ContactForm";
 import { Link } from "gatsby";
 import Modal from "../../components/modal";
 
-// Importa la componente SEO
-import SEO from "../../components/SEO";
+
 
 const PrestamosPersonales = () => {
     const [loanAmount, setLoanAmount] = useState(1000)
