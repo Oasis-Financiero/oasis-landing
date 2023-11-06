77ad4578ee90a3026ae0b00b051d7bea1e225bd1
OasisFintech <63435165+OasisFintech@users.noreply.github.com>
2023-11-05 19:27:32 -0600

    Update gatsby-config.js
    Se quitan los datos en sitemetadata para corregir conflicto con la rama dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,34 +9,7 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Compara préstamos, seguros y aprende sobre ahorro, inversión y finanzas.`,
-    siteUrl: `https://www.oasisfinanciero.com`,
-    description: `Compara préstamos y seguros, y adquiere conocimientos sobre ahorro, inversión y finanzas en Oasis Financiero, tu socio en el camino hacia la estabilidad financiera.`,
-    author: `@edchavezb`,
-    keywords: [
-      'ia',
-      'ahorro',
-      'finanzas',
-      'asesor',
-      'financiero',
-      'inversión',
-      'crédito',
-      'calculadora',
-      'deuda',
-      'seguro',
-      'seguros',
-      'prestamos',
-      'préstamos',
-      'seguro de vida',
-      'educación financiera',
-      'finanzas personales',
-      'información financiera',
-      'prestamos personales',
-      'préstamo personal',
-      'seguros de auto',
-      'mejores préstamos personales en México',
-      'mejores seguros de auto en México',
-      'préstamos personales en línea',
+ 
     ]
   },
   plugins: [
