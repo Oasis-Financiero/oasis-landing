957a2e70b16a0d538000b458bd7975c00f1c3c4d
OasisFintech <63435165+OasisFintech@users.noreply.github.com>
2024-09-10 20:20:20 -0600

    Update index.js
    Se importa el hook useEffect de React, que es necesario para ejecutar el código que carga el script del chatbot.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import Modal from "../components/modal";
